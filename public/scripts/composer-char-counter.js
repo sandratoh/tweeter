5a65b8e1e98ef9a0865b6f213978d4e392f4f343
Sandra To <sandra.to.e@gmail.com>
2021-02-24 01:22:08 +0000

    Update input event handler to change char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
 
   $('#tweet-text').on('input', function(event) {
-    let count = $('#tweet-text').val();
-    console.log(count.length);
-    console.log($(this).closest('form').find('.counter'));
+    const tweetLength = $('#tweet-text').val().length;
+    const counter = $(this).closest('form').find('.counter');
+    const maxChar = 140;
+
+    counter.text(maxChar - tweetLength);
   });
-  
 });
