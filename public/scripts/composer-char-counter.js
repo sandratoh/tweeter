7ab27d4ad21087eb6240645f3dc1acb967fb11dc
Sandra To <sandra.to.e@gmail.com>
2021-02-24 02:19:00 +0000

    Change counter color according to tweet char

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,5 +6,30 @@ $(document).ready(() => {
     const maxChar = 140;
 
     counter.text(maxChar - tweetLength);
+
+    // if (tweetLength >= 120) {
+    //   counter.css('color', '#F3A712');
+    // }
+    
+    // if (tweetLength > 140) {
+    //   counter.css('color', '#EE5862');
+    // }
+
+    // if (tweetLength < 120) {
+    //   counter.css('color', '#353D46');
+    // }
+
+    if (tweetLength >= 120) {
+      counter.addClass('char-warning');
+    }
+    
+    if (tweetLength > 140) {
+      counter.removeClass('char-warning');
+      counter.addClass('char-invalid');
+    }
+
+    if (tweetLength < 120) {
+      counter.removeClass(['char-warning', 'char-invalid']);
+    }
   });
 });
