0ba5dc53edf3bce87600f4134f6e93bf5998fed8
Sandra To <sandra.to.e@gmail.com>
2021-02-24 02:28:22 +0000

    Remove prev noted out counter color change codes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,18 +7,6 @@ $(document).ready(() => {
 
     counter.text(maxChar - tweetLength);
 
-    // if (tweetLength >= 120) {
-    //   counter.css('color', '#F3A712');
-    // }
-    
-    // if (tweetLength > 140) {
-    //   counter.css('color', '#EE5862');
-    // }
-
-    // if (tweetLength < 120) {
-    //   counter.css('color', '#353D46');
-    // }
-
     if (tweetLength >= 120) {
       counter.addClass('char-warning');
     }
