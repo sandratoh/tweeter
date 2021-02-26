5b8db4532b6408472a296bcffe4db1cccbf89b15
Sandra To <sandra.to.e@gmail.com>
2021-02-26 07:35:14 +0000

    Move helper func to separate js file to modularize client

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,58 +1,8 @@
 /* global $ */
 /* global document */
+/* global timeInterval */
 
 $(document).ready(() => {
-  
-  const timeInterval = postTime => {
-    const currentTime = Date.now();
-    const msPerMin = 1000 * 60;
-    const msPerHour = msPerMin * 60;
-    const msPerDay = msPerHour * 24;
-    const msTimeDiff = currentTime - postTime;
-
-    let num = msTimeDiff;
-    let unit = '';
-
-    if (msTimeDiff < msPerMin) {
-      num /= 1000;
-      unit = 'second';
-      
-    } else if (msTimeDiff < msPerHour) {
-      num /= msPerMin;
-      unit = 'minute';
-
-    } else if (msTimeDiff < msPerDay) {
-      num /= msPerHour;
-      unit = 'hour';
-
-    } else {
-      const numOfDays = (msTimeDiff) / (msPerDay);
-      
-      num = numOfDays;
-      unit = 'day';
-      
-      if (numOfDays >= 365) {
-        num /= 365;
-        unit = 'year';
-  
-      } else if (numOfDays >= 30) {
-        num /= 30;
-        unit = 'month';
-  
-      } else if (numOfDays >= 7) {
-        num /= 7;
-        unit = 'week';
-      }
-      
-    }
-    let roundedNum = Math.floor(num);
-    
-    if (roundedNum > 1) {
-      unit += 's';
-    }
-    
-    return `${roundedNum} ${unit} ago`;
-  };
 
   const renderTweets = tweets => {
     $('#tweets-container').empty();
