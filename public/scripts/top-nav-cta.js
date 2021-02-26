1c0212a781111e19e2d41eb26fb6d3ae535bed91
Sandra To <sandra.to.e@gmail.com>
2021-02-26 07:23:13 +0000

    Add id and codes to enable top-nav auto scrolling

--- public/scripts/top-nav-cta.js
+++ public/scripts/top-nav-cta.js
@@ -4,5 +4,11 @@
 $(document).ready(() => {
   $('.nav-cta').on('click', () => {
     $('textarea').focus();
+    
+    const tweetArea = document.getElementById('tweet-area');
+    tweetArea.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    });
   });
 });
